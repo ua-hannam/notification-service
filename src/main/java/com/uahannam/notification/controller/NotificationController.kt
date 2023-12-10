fc14ca47d678da9d082b8b17a4d41c253a2e13cd
서민재 <seominjae@seominjaeui-Macmini.local>
2023-12-10 23:12:44 +0900

    ⚙️ chore: handler -> controller 명칭 변경(폴더 포함)

--- src/main/java/com/uahannam/notification/controller/NotificationController.kt
+++ src/main/java/com/uahannam/notification/controller/NotificationController.kt
@@ -1,4 +1,4 @@
-package com.uahannam.notification.handler
+package com.uahannam.notification.controller
 
 import com.uahannam.notification.service.PushService
 import org.springframework.http.HttpStatus
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class NotificationHandler(
+class NotificationController(
     private val pushService: PushService
 ) {
 
