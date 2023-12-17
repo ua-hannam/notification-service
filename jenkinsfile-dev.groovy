00637c4eb49f7e3bb4e1f505a5c816b0f5683337
Sungmin Kim <83997823+keeptrynjoy@users.noreply.github.com>
2023-12-17 13:14:54 +0900

    Update jenkinsfile-dev.groovy

--- jenkinsfile-dev.groovy
+++ jenkinsfile-dev.groovy
@@ -1,16 +1,24 @@
 def app
 
 node {
-    stage('Checkout') {
-        checkout scm 
-    }
+    try {
+        notifySlack("STARTED", "#0000FF")
+    
+        stage('Checkout') {
+            checkout scm 
+        }
+    
+        stage('Ready') {      
+            echo 'Ready to build' 
+            gradleHome = tool 'gradle'   
+        }
+    
+        stage('Build') {
+            sh "${gradleHome}/bin/gradle clean build -x test"
+        }
 
-    stage('Ready') {      
-        echo 'Ready to build' 
-        gradleHome = tool 'gradle'   
-    }
-
-    stage('Build') {
-        sh "${gradleHome}/bin/gradle clean build -x test"
-    }
+        notifySlack("SUCCESS", "#00FF00")
+ 	} catch(e) {
+        notifySlack("FAILED", "#FF0000")
+	}
 }
