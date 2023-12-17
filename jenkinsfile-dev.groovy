246ea8fa46604bb9f14e48dcbef61d1776340281
Sungmin Kim <83997823+keeptrynjoy@users.noreply.github.com>
2023-12-17 15:24:27 +0900

    Update jenkinsfile-dev.groovy

--- jenkinsfile-dev.groovy
+++ jenkinsfile-dev.groovy
@@ -3,7 +3,7 @@ def app
 node {
     try{
     slackSend(channel: '#backend-bulid-log', message: """
-*Build start* -- *${env.JOB_NAME}* - *[#${env.BUILD_NUMBER}]*
+*Build start(${env.JOB_NAME}[#${env.BUILD_NUMBER}])*
 """)
         
     stage('Checkout') {
