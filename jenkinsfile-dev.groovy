f03e36f735a352abd92de5c120f8aa19d2195795
Sungmin Kim <83997823+keeptrynjoy@users.noreply.github.com>
2023-12-17 15:55:44 +0900

    Update jenkinsfile-dev.groovy

--- jenkinsfile-dev.groovy
+++ jenkinsfile-dev.groovy
@@ -1,28 +1,9 @@
 def app
-blocks = [
-	[
-		"type": "section",
-		"text": [
-			"type": "mrkdwn",
-			"text": "Hello, Assistant to the Regional Manager Dwight! *Michael Scott* wants to know where you'd like to take the Paper Company investors to dinner tonight.\n\n *Please select a restaurant:*"
-		]
-	],
-    [
-		"type": "divider"
-	],
-	[
-		"type": "section",
-		"text": [
-			"type": "mrkdwn",
-			"text": "*Farmhouse Thai Cuisine*\n:star::star::star::star: 1528 reviews\n They do have some vegan options, like the roti and curry, plus they have a ton of salad stuff and noodles can be ordered without meat!! They have something for everyone here"
-		],
-		"accessory": [
-			"type": "image",
-			"image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/c7ed05m9lC2EmA3Aruue7A/o.jpg",
-			"alt_text": "alt text for image"
-		]
-	]
-]
+def notifySlack(STATUS, COLOR) {
+    slackSend channel: '#채널명', 
+	message: STATUS+" : " + "${env.JOB_NAME}[${env.BUILD_NUMBER}] <${env.BUILD_URL}|OPEN>", 
+	color: COLOR teamDomain: 'uahan'
+}
 
 
 node {
@@ -43,13 +24,9 @@ node {
         sh "${gradleHome}/bin/gradle clean build -x test"
     }
     
-    slackSend(channel: '#backend-bulid-log', color: '#00FF00', blocks: blocks)
+    notifySlack("SUCCESS", "#00FF00")
 
     } catch (Exception e) {
-        slackSend(channel: '#backend-bulid-log', color: 'danger', message: """ 
-*Build failed*
-- Job : _${env.JOB_NAME}_[#${env.BUILD_NUMBER}] <${env.BUILD_URL}|OPEN>
-} """)
+	notifySlack("FAILED", "#FF0000")
     }
-    
 }
