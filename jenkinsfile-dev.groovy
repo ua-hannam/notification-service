044a901c512f84a53841cf1c12ced6ac9aa1cf83
Sungmin Kim <83997823+keeptrynjoy@users.noreply.github.com>
2023-12-17 14:59:43 +0900

    Update jenkinsfile-dev.groovy

--- jenkinsfile-dev.groovy
+++ jenkinsfile-dev.groovy
@@ -1,6 +1,8 @@
 def app
 
 node {
+    try{
+    slackSend(channel: '#backend-bulid-log', message: 'Build start')
     stage('Checkout') {
         checkout scm 
     }
@@ -12,5 +14,12 @@ node {
 
     stage('Build') {
         sh "${gradleHome}/bin/gradle clean build -x test"
+    }
+
+    slackSend(channel: '#backend-bulid-log', message: 'Build successful')
+
+    } catch (Exception e) {
+        slackSend(channel: '#backend-bulid-log', message: 'Build failed: ' + e.getMessage())
+        throw e 
     }
 }
